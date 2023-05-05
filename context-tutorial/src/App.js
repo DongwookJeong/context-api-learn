04e314a6f08371d04d379ca506b3eacd7288bbf2
DongwookJeong <nolhoon211@gmail.com>
2023-05-05 13:26:19 +0900

    context API 익히기, 전역적으로 사용할 데이터가 있을 때 유용

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,11 @@
+import ColorBox from "./components/colorBox";
+
+const App = () => {
+  return (
+    <div>
+      <ColorBox/>
+    </div>
+  )
+}
+
+export default App;
---
+++ context-tutorial/src/components/colorBox.js
@@ -0,0 +1,19 @@
+import ColorContext from "../context.js/color";
+
+const ColorBox = () => {
+    return (
+         <ColorContext.Consumer>
+            {value => (
+                <div
+                    style={{
+                        width: '64px',
+                        height: '64px',
+                        background: value.color
+                    }}
+                />
+            )}
+        </ColorContext.Consumer>
+    )
+}
+
+export default ColorBox
